26103a00aa80602844de8514e9e08c20019836ed
aliefdany <works@alief.tech>
2024-11-18 23:53:35 +0700

    generated watches resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WatchesModule } from './watches/watches.module';
 
 @Module({
-  imports: [],
+  imports: [WatchesModule],
   controllers: [AppController],
   providers: [AppService],
 })
