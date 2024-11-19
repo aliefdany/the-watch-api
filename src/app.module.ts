380fdbaed7ed4007be4447fbc0ffcd9544368927
aliefdany <works@alief.tech>
2024-11-20 03:08:23 +0700

    installed passport and jwt packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WatchesModule } from './watches/watches.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [WatchesModule, PrismaModule],
+  imports: [WatchesModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
