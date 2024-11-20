890d1c73c726710ab3cae18d59f21133f3d56387
aliefdany <works@alief.tech>
2024-11-20 14:50:28 +0700

    implemented unit test for auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,22 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaService } from '..//prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
+import { SignInDto } from './dto/signin.auth.dto';
+import { AuthEntity } from './entities/auth.entity';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
+
+  const mockAuthService = {
+    signin: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, PrismaService, JwtService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return an access token when valid credentials are provided', async () => {
+      const signInDto: SignInDto = {
+        username: 'testuser',
+        password: 'password123',
+      };
+
+      const expectedAuthEntity: AuthEntity = { accessToken: 'testAccessToken' };
+      mockAuthService.signin.mockResolvedValue(expectedAuthEntity);
+
+      const result = await authController.login(signInDto);
+      expect(result).toEqual(expectedAuthEntity);
+      expect(authService.signin).toHaveBeenCalledWith(
+        signInDto.username,
+        signInDto.password,
+      );
+    });
   });
 });
