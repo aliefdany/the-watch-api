9868cbcf58916412ab9c8dcd2f386518026e06aa
aliefdany <works@alief.tech>
2024-11-21 12:14:24 +0700

    added simple rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { WatchesModule } from './watches/watches.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './accounts/account.module';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { CustomValidationPipe } from './custom-validation.pipe';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ validate, isGlobal: true }),
     CacheModule.register({ ttl: 60 * 1000, isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 10 * 1000, limit: 5 }]),
     WatchesModule,
     PrismaModule,
     AuthModule,
@@ -30,6 +32,10 @@ import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
     {
       provide: APP_INTERCEPTOR,
       useClass: CacheInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
